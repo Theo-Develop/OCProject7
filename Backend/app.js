85b250d7f211aaa1f0c623351ba29cac3c4fd007
Théo Dressy <t.dressy@gmail.com>
2024-03-02 10:11:19 +0100

    adding put and delete

--- Backend/app.js
+++ Backend/app.js
@@ -35,13 +35,25 @@ app.post("/api/books", (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/books/:id', (req, res, next) => {
+app.put("/api/books/:id", (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
     Book.findOne({ _id: req.params.id })
         .then(book => res.status(200).json(book))
         .catch(error => res.status(404).json({ error }));
 });
 
-app.get("/api/stuff", (req, res, next) => {
+app.get("/api/books", (req, res, next) => {
     Book.find()
         .then(books => res.status(200).json(books))
         .catch(error => res.status(400).json({ error }));

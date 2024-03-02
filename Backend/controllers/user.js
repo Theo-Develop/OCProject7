45f0bbe84daa11716e0e00ecaae1a1c3461ffb48
Théo Dressy <t.dressy@gmail.com>
2024-03-02 17:07:49 +0100

    settings jwt

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
@@ -29,7 +31,11 @@ exports.login = (req, res, next) => {
                         } else {
                             res.status(200).json({
                                 userId: user._id,
-                                token: "TOKEN"
+                                token: jwt.sign(
+                                    { userId: user._id },
+                                    "RANDOM_TOKEN_SECRET",
+                                    { expiresIn: "24H" }
+                                )
                             });
                         }
                     })

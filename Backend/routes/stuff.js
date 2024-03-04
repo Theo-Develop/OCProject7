ebff980116272872196dcac401318b91c08303ba
Théo Dressy <t.dressy@gmail.com>
2024-03-04 08:04:43 +0100

    settings authorization middleware

--- Backend/routes/stuff.js
+++ Backend/routes/stuff.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
+const auth = require("../middleware/auth");
+
 const stuffCtrl = require("../controllers/stuff");
 
-router.post("/", stuffCtrl.createBooks);
-router.put("/:id", stuffCtrl.modifyBooks);
-router.delete("/:id", stuffCtrl.deleteBooks);
+router.post("/", auth, stuffCtrl.createBooks);
+router.put("/:id", auth, stuffCtrl.modifyBooks);
+router.delete("/:id", auth, stuffCtrl.deleteBooks);
 router.get("/:id", stuffCtrl.readOneBook);
 router.get("/", stuffCtrl.readAllBooks);
 

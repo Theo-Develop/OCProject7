a8de70da1878713af8dd579eb61031f0215d7fc7
Théo Dressy <t.dressy@gmail.com>
2024-03-04 10:48:20 +0100

    settings token

--- Backend/routes/stuff.js
+++ Backend/routes/stuff.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const stuffCtrl = require("../controllers/stuff");
 
 router.post("/", auth, stuffCtrl.createBooks);
 router.put("/:id", auth, stuffCtrl.modifyBooks);
 router.delete("/:id", auth, stuffCtrl.deleteBooks);
-router.get("/:id", auth, stuffCtrl.readOneBook);
-router.get("/", auth, stuffCtrl.readAllBooks);
+router.get("/:id", stuffCtrl.readOneBook);
+router.get("/", stuffCtrl.readAllBooks);
 
 module.exports = router;
